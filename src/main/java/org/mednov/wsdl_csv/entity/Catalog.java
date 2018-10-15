b6844e89a4c73ab31c5815d537880d49338ae919
mac <mac@MacBook-Air-mac-2.local>
2018-10-15 23:32:47 +0300

    Entities added

---
+++ src/main/java/org/mednov/wsdl_csv/entity/Catalog.java
@@ -0,0 +1,39 @@
+package org.mednov.wsdl_csv.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class Catalog {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+
+    private String code;
+
+    @OneToMany(mappedBy = "catalog", fetch = FetchType.LAZY)
+    private List<Requests> requests;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public List<Requests> getRequests() {
+        return requests;
+    }
+
+    public void setRequests(List<Requests> requests) {
+        this.requests = requests;
+    }
+}
---
+++ src/main/java/org/mednov/wsdl_csv/entity/Requests.java
@@ -0,0 +1,61 @@
+package org.mednov.wsdl_csv.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Requests {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "CODE_ID", nullable = false)
+    private Catalog catalog;
+
+    private int number;
+
+    private String fileNames;
+
+    private String error;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Catalog getCatalog() {
+        return catalog;
+    }
+
+    public void setCatalog(Catalog catalog) {
+        this.catalog = catalog;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getFileNames() {
+        return fileNames;
+    }
+
+    public void setFileNames(String fileNames) {
+        this.fileNames = fileNames;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
