69d1f1e070ffa7abbfa91ae5681460521fe88da8
mac <mac@MacBook-Air-mac-2.local>
2018-10-16 18:51:08 +0300

    Repositories added

---
+++ src/main/java/org/mednov/wsdl_csv/repository/CatalogRepository.java
@@ -0,0 +1,8 @@
+package org.mednov.wsdl_csv.repository;
+
+import org.mednov.wsdl_csv.entity.Catalog;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CatalogRepository extends CrudRepository<Catalog, Integer> {
+
+}
---
+++ src/main/java/org/mednov/wsdl_csv/repository/RequestsRepository.java
@@ -0,0 +1,7 @@
+package org.mednov.wsdl_csv.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RequestsRepository extends CrudRepository {
+
+}
